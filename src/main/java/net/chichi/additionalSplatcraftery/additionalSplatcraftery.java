273209e281ff77d26703a2a171e996acb4c8c794
Chichi <christperez1216@gmail.com>
2026-07-14 13:30:29 -0400

    Added example item

--- src/main/java/net/chichi/additionalSplatcraftery/additionalSplatcraftery.java
+++ src/main/java/net/chichi/additionalSplatcraftery/additionalSplatcraftery.java
@@ -1,10 +1,12 @@
 package net.chichi.additionalSplatcraftery;
 
 import com.mojang.logging.LogUtils;
+import net.chichi.additionalSplatcraftery.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -29,8 +31,12 @@ public class additionalSplatcraftery
 
     public additionalSplatcraftery()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
